dfb991249a0d041de934998c0dc4d9f77974bc34
Jeielsantosdev <jeielsantos.ti@gmail.com>
2025-09-08 07:29:37 -0300

    encripted with xor

--- xor.py
+++ xor.py
@@ -4,47 +4,58 @@ no mesmo programa adicione a opção de ler um arquivo que desfaça o xor e conv
 
 
 def encrypt_text(text, key):
-    ciphertext_nums = []
+    encrypted_bytes = bytearray()
     for i, char in enumerate(text):
         key_char = key[i % len(key)]
         original_char_code = ord(char)
         code_char_key = ord(key_char) 
         encrypted_code = original_char_code ^ code_char_key
-        ciphertext_nums.append(encrypted_code)
-    return ciphertext_nums
+        encrypted_bytes.append(encrypted_code)
+    return bytes(encrypted_bytes)
 
-def decipher_numbers(list_numbers, key):
-    
+def decipher_text(cipher_bytes, key):
     deciphered_text = ""
-    for i, num in enumerate(list_numbers):
+    for i, byte in enumerate(cipher_bytes):
         key_char = key[i % len(key)]
         code_char_key = ord(key_char)
-        encrypted_code = num ^ code_char_key
-        deciphered_text += chr(encrypted_code)
+        decrypted_code = byte ^ code_char_key
+        deciphered_text += chr(decrypted_code)
     return deciphered_text
 
 def salvar_arquivo(file_name, encrypted_data):
-   
     try:
         with open(file_name, 'wb') as f:
-            f.write(bytes(encrypted_data))
-        print(f"\n Sucesso Mensagem cifrada salva em '{file_name}'")
+            f.write(encrypted_data)
+        print(f"\n Sucesso: Mensagem cifrada salva em '{file_name}'")
     except IOError as e:
-        print(f"\n Erro  Não foi possível salvar o arquivo: {e}")
+        print(f"\n Erro: Não foi possível salvar o arquivo: {e}")
 
 def read_file(file_name):
-    
     try:
         with open(file_name, 'rb') as f:
             data_bytes = f.read()
-            list_numbers = list(data_bytes)
-            print(f"\nSucesso Arquivo '{file_name}' lido.")
-            return list_numbers
+            print(f"\n Sucesso: Arquivo '{file_name}' lido.")
+            return data_bytes
     except FileNotFoundError:
-        print(f"\n Erro Arquivo '{file_name}' não encontrado.")
+        print(f"\n Erro: Arquivo '{file_name}' não encontrado.")
         return None
     except IOError as e:
-        print(f"\n Erro Não foi possível ler o arquivo: {e}")
+        print(f"\n Erro: Não foi possível ler o arquivo: {e}")
         return None
+
+
+# Exemplo de uso
+
+msg = "jeiel"
+key = "123"
+
+
+encrypted = encrypt_text(msg, key)
+salvar_arquivo("ab.bin", encrypted)
+print("codificada: ", encrypted)
+
+loaded = read_file("ab.bin")
+if loaded:
+    print(" Decifrada:", decipher_text(loaded, key))
 
 
